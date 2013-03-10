8b0d19f3155f4cc0d67feb528a58014e2fa514a5
Steven Rosenberg <steven@stevenrosenberg.net>
2013-03-09 22:28:17 -0800

    Clarifying the notes above the script

--- usb-backup.sh
+++ usb-backup.sh
@@ -9,8 +9,13 @@
 # The destination drive is in the path /media/ because this is where the GNOME 2.x/3.x
 # desktop environment puts USB drives that are plugged into the system.
 #
-# I named my backup drive 'Linux.' Yours may very well have a different name. Adjust your script accordingly.
+# I named my backup drive 'Linux,' and within that drive and am
+# backing up everyting to a directory named 'debian' Your path may very
+# well have a different name. Adjust your script accordingly.
 #
-# The path to my exclude file -- /home/steven/Documents/shell_scripts/debian-squeeze/exclude -- is very likely different from yours. Adjust your script accordingly.
+# The path to my exclude file is:
+# /home/steven/Documents/shell_scripts/debian-squeeze/exclude
+# It is very likely different from yours. Adjust your script accordingly.
+#
 rsync -av --delete --exclude-from '/home/steven/Documents/shell_scripts/debian-squeeze/exclude' /home /media/Linux/debian
 exit 0
