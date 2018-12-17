7745d19aace7a9e3a148c4ae0864004237c3e10f
Steven Rosenberg <stevenhrosenberg@gmail.com>
2018-12-16 17:20:21 -0800

    Create rsync_windows_from_wsl.sh

---
+++ rsync_windows_from_wsl.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# This script uses the rsync from the Windows Subsystem for Linux in Windows 10 to backup files on the Windows side of the system
+
+# While working on your WSL (in my case Ubuntu) files with Windows programs can cause problems, there is supposedly nothing wrong with using programs in the WSL to modify files on the Windows side.
+
+# For test purposes, I am syncing the contents of one directory in my current working directory to another directory in that same working directory
+
+# The slash after the first directory (the "sync from" directory) means sync the contents and not the directory itself. Remove the slash to copy this entire directory, including the directory itself, into the target (or "to") directory
+
+rsync -av --delete rsync_from/ rsync_to
+
+exit 0
