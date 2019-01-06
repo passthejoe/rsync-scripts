ff9c3fe621aec04c8e4e8f9bd1d7a86701feff7d
Steven Rosenberg <stevenhrosenberg@gmail.com>
2019-01-05 21:59:13 -0800

    Create rsync_with_variables.sh

---
+++ rsync_with_variables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# rsync script using Bash variables
+
+# Put the directory you wish to copy from between the quotation marks.
+# End with a "slash" (the / character) if you want to copy
+# the contents of the directory but not the directory itself.
+#
+# End without a slash if you DO want to copy the directory
+# and all of its contents.
+#
+# Example:
+# copy_from="/home/steven/"
+copy_from="rsync_from/"
+
+# Put the directory you wish to copy to between the quotation marks
+# Example:
+# copy_to="/run/media/steven/Linux/fedora"
+copy_to="rsync_to"
+
+rsync -av --delete $copy_from $copy_to
+
+exit 0
