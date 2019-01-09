725309bca5eddf2f858bef99bf6929801c44d598
Steven Rosenberg <stevenhrosenberg@gmail.com>
2019-01-09 15:45:54 -0800

    Adding titles in the comments

--- rsync_with_variables.sh
+++ rsync_with_variables.sh
@@ -2,6 +2,7 @@
 
 # rsync script using Bash variables
 
+# Copy From
 # Put the directory you wish to copy from between the quotation marks.
 # End with a "slash" (the / character) if you want to copy
 # the contents of the directory but not the directory itself.
@@ -11,11 +12,14 @@
 #
 # Example:
 # copy_from="/home/steven/"
+
 copy_from="rsync_from/"
 
+# Copy To
 # Put the directory you wish to copy to between the quotation marks
 # Example:
 # copy_to="/run/media/steven/Linux/fedora"
+
 copy_to="rsync_to"
 
 rsync -av --delete $copy_from $copy_to
